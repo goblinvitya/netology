98ef7d77f53c5aa1cf7ee336dda87b85e0670355
daniel cromers <128777207+goblinvitya@users.noreply.github.com>
2023-12-01 21:18:49 +0500

    first commit

---
+++ src/main/java/ru/netology/kochnev_danil/OperationHistoryApiApplication.java
@@ -0,0 +1,11 @@
+package ru.netology.kochnev_danil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationHistoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationHistoryApiApplication.class);
+    }
