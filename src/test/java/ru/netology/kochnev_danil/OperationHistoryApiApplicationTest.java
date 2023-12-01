b52583715ac53e309a5ff3e01a31abd7ab79d7c8
daniel cromers <128777207+goblinvitya@users.noreply.github.com>
2023-12-01 21:03:52 +0500

    first commit

---
+++ src/test/java/ru/netology/kochnev_danil/OperationHistoryApiApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.netology.kochnev_danil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OperationHistoryApiApplicationTest {
+    @Test
+    void contextLoads(){
+    }
+
+}
